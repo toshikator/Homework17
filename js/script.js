11c86854b5453e516ad4ba3f3c64a98bf5ab2cf4
Anton Bukhman <diamondspb2015@gmail.com>
2023-01-02 16:21:45 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function  main(){
+
+    let travelDatabase = [];
+    const loadTravelDatabase = ()=>{
+        travelDatabase = JSON.parse(localStorage.getItem('travelDatabase'));
+    }
+    const saveTravelDatabase = ()=>{
+        localStorage.removeItem('travelDatabase');
+        localStorage.setItem('travelDatabase',JSON.stringify(travelDatabase))
+    }
+    const addItemToArray = (array,item)=>{
+        array.push(item);
+    }
+    const saveCurrentItemToDatabase = (currentItem)=>{
+        addItemToArray(travelDatabase,currentItem)
+    }
+
+
+
+
+
+}())
