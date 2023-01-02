bd7728eefc9e6634857f71ccbed88dbf282234f1
Anton Bukhman <diamondspb2015@gmail.com>
2023-01-02 21:52:30 +0200

    localStorage ready

--- js/script.js
+++ js/script.js
@@ -1,23 +1,51 @@
+
 (function  main(){
 
     let travelDatabase = [];
+    let item = {};
+    const formElement = document.getElementById('myForm');
+    formElement.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(formElement);
+        item['cityFrom'] = formData.get('cityFrom');
+        item['cityTo'] = formData.get('cityTo');
+        item['budget'] = formData.get('budget');
+        item['dateStart'] = formData.get('dateStart');
+        item['dateEnd'] = formData.get('dateEnd');
+        item['amountPersons'] = formData.get('amountPersons');
+        item['mainTransferType'] = formData.get('mainTransferType');
+        if(item['dateEnd'] < item['dateStart']) [item['dateStart'],item['dateEnd']] = [item['dateEnd'],item['dateStart']];
+        console.log(item);
+        addItemToTravelDatabase(item);
+        console.log(travelDatabase);
+    });
 
-    const loadTravelDatabase = ()=>{
-        travelDatabase = JSON.parse(localStorage.getItem('travelDatabase'));
+    function saveTravelDatabaseToLocalStorage() {
+        localStorage.removeItem("travelDatabase");
+        localStorage.setItem("travelDatabase",JSON.stringify(travelDatabase));
     }
-    const saveTravelDatabase = ()=>{
-        localStorage.removeItem('travelDatabase');
-        localStorage.setItem('travelDatabase',JSON.stringify(travelDatabase))
-    }
-    const addItemToArray = (array,item)=>{
-        array.push(item);
-    }
-    const saveCurrentItemToDatabase = (currentItem)=>{
-        addItemToArray(travelDatabase,currentItem)
+
+    function loadDatabaseFromLocalStorage() {
+        const storageVal = localStorage.getItem("travelDatabase");
+        // console.log(storageVal);
+        travelDatabase = (storageVal)?JSON.parse(storageVal):[];
+        console.log(travelDatabase);
     }
 
+    function addItemToTravelDatabase(item) {
+    travelDatabase.push(item);
+    saveTravelDatabaseToLocalStorage();
+    }
+
+    function makeTravelList(array) {
+
+
+        return result;
+    }
 
 
 
+    loadDatabaseFromLocalStorage();
+    console.log(travelDatabase);
 
 }())
